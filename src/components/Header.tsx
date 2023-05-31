f30effa91662ec9b08ec17accb78d642053ffae4
sanmoyano <smoyano.di@gmail.com>
2023-05-31 14:32:35 -0300

    styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Header = () => {
   return (
     <div className='flex justify-between align-middle'>
-      <h1 className='text-xl font-mono font-bold text-white'>Dev-Finder</h1>
+      <h1 className='text-2xl font-mono font-bold text-white'>DevFinder</h1>
       <FontAwesomeIcon fontSize='2rem' icon={faGithub} />
     </div>
   )
